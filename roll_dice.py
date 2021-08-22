34b102a8812ffda57ff820edb350118824ae2842
Ray <ray247k@gmail.com>
2021-08-22 19:22:45 +0800

    Add dice bot function

--- roll_dice.py
+++ roll_dice.py
@@ -1,6 +1,9 @@
 # 導入 Discord.py 套件
 import discord
 
+# 導入隨機數套件
+import random
+
 # 取得 Discord client 物件才能操作
 client = discord.Client()
 
@@ -19,10 +22,67 @@ async def on_message(message):
     # 排除機器人本身發出的訊息，避免機器人自問自答的無限迴圈
     if message.author == client.user:
         return
-    
-    # 如果我們說了「誰是機器人」，機器人就會回「誰叫我？」
-    if message.content == '誰是機器人':
-        await message.channel.send('誰叫我？')
+
+    # 預設錯誤訊息
+    error = []
+
+    # 處理輸入文字
+    content = message.content.replace(' ', '').lower()
+
+    # 如果是「!roll」開頭的訊息
+    if message.content.startswith('!roll'):
+        content = content.replace('!roll', '')
+
+        # 骰子數量計算
+        dice_cont = content.split('d')[0]
+
+        try:
+            if isinstance(int(dice_cont), int):
+                dice_cont = int(dice_cont)
+
+        except ValueError:
+            error.append('how many dice you roll must be an interger!')
+
+        # 骰子類型判斷
+        content = content.split('d')[1]
+        dice_type = content.split('>')[0]
+        try:
+            if isinstance(int(dice_type), int):
+                dice_type = int(dice_type)
+
+        except ValueError:
+            error.append('dice type must be an interger!')
+
+        # 成功判斷
+        if '>' in content:
+            success = content.split('>')[1]
+            try:
+              if isinstance(int(success), int):
+                success = int(success)    
+            except ValueError:
+                error.append('success condition must be an interger!')
+
+        else:
+            success = 0
+
+        if len(error) == 0:
+            success_count = 0
+            result_msg = ''
+
+            # 擲骰子
+            results = [random.randint(1, dice_type) for _ in range(dice_cont)]
+
+            for result in results:
+                if success > 0 and result >= success:
+                    success_count += 1
+                result_msg += f'`{result}`, '
+            
+            await message.channel.send(result_msg)
+
+            if success > 0:
+                await message.channel.send(f'Success: `{success_count}`')
+        else:
+            await message.channel.send(error)
 
 # TOKEN 在 Discord Developer 的「BOT」頁取得
 client.run('')
